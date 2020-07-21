428ab366c4dce6f03145dc14a03c733b7fae7e56
CarlosSe <carlos.seggch@gmail.com>
2020-07-21 18:15:46 -0500

    args parser refactor and methods output
    Co-authored-by: Raul Alejandro Almanza Serrano <ra.almanzaserrano@gmail.com>

--- bin/args_parser.rb
+++ bin/args_parser.rb
@@ -1,39 +1,35 @@
 # frozen_string_literal: true
 
+VALID = { 'port' => '-p', 'logging' => '-l', 'directory' => '-d' }.freeze
+
 # This class verify that the arguments match the schema
 class ArgsParser
-  VALID = {"port" => "-p", "loggin" => "-l", "directory" => "-d"}
-
-  def start
-    print "Ingrese argumentos: "
-    args = gets().split(" ")
-    parser(args)
+  def initialize(statement)
+    @arguments = statement
   end
 
-  def parser(args)
-    elem = []
-    args.each do |arg|
-      if arg.start_with?('-')
-        raise "This paramter is invalid: #{arg}" unless check_valid?(arg)
-        elem.append(arg)
-      end
-      execute_method(elem.append(arg))
+  def start
+    @arguments.each_with_index do |arg, idx|
+      next unless arg.start_with?('-')
+
+      print 'Argumento no valido' unless VALID.value?(arg)
+      retrieve_value(VALID.key(arg), idx + 1)
     end
   end
 
-  def check_valid?(arg)
-    VALID.value?(arg)
+  def retrieve_value(arg_value, idx)
+    current_argument = @arguments[idx]
+    return unless current_argument
+
+    puts flag?(idx) ? "#{arg_value} : true" : "#{arg_value} : #{current_argument}"
   end
 
-  def execute_method(elems)
-    self.public_send(VALID.key(elems.first), elems.last)
-  end
-
-  def port(port)
-    puts "Running on port #{port}"
+  def flag?(idx)
+    @arguments[idx].start_with?('-')
   end
 end
 
-parser = ArgsParser.new
+print 'Ingresa argumentos: '
+statement = gets.split(' ')
+parser = ArgsParser.new(statement)
 parser.start
-
