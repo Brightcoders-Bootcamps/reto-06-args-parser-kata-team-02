cdc1b55ecb6d90b9a0aa251cb99969acdc2a7cda
Raul Almanza <ra.almanzaserrano@gmail.com>
2020-07-22 22:03:43 -0500

    Refactoring classes to achieve challenge goals

--- bin/args_parser.rb
+++ bin/args_parser.rb
@@ -4,50 +4,35 @@ require_relative 'schema'
 
 # This class verify that the arguments match the schema
 class ArgsParser
-  def initialize(statement)
-    @arguments = statement
+  def initialize
     @schema = Schema.new
   end
 
-  def start
-    @arguments.each_with_index do |arg, index|
-      next unless arg.start_with?('-')
-      unless @schema.valid_schema[arg]
-        print "Bandera #{arg} no valida"
-        return
-      end
-      verify_arg(arg, index + 1)
+  def start(arguments)
+    elements = @schema.unpack_values(arguments)
+    if elements.empty?
+      puts 'Invalid argument'
+      return
     end
+    elements.each { |arg| display_result(arg[1], arg[2], arg[3]) }
   end
 
-  def verify_arg(arg, next_index)
-    next_arg = @arguments[next_index]
-    return unless next_arg
-    type = get_type(next_arg)
-    expected = @schema.valid_schema[arg]
-    if valid_arg?(arg, type) 
-      print "#{expected.first}: ", flag?(next_arg) ? 'true' : next_arg.to_s, "\n"
-    else
-      puts "Error Type: flag #{arg} should be type #{expected.last}, #{type} given."
-    end
+  def display_result(method, type, value = nil)
+    return "INV ARG TYPE: #{value.class}, expencted #{type}" if value && invalid_type?(type, value)
+
+    print("#{method}: ", trueclass?(type) ? 'true' : value.to_s, "\n")
   end
 
-  def get_type(next_arg)
-    return TrueClass if flag?(next_arg)
-    return Integer if next_arg.to_i != 0
-    String
+  def invalid_type?(type, value)
+    type != value&.class
   end
 
-  def valid_arg?(arg, type)
-    true if @schema.valid_schema[arg].last == type
-  end
-
-  def flag?(arg)
-    arg.start_with?('-')
+  def trueclass?(arg)
+    arg == TrueClass
   end
 end
 
 print 'Ingresa argumentos: '
 statement = gets.split(' ')
-parser = ArgsParser.new(statement)
-parser.start
+parser = ArgsParser.new
+parser.start(statement)
--- bin/schema.rb
+++ bin/schema.rb
@@ -4,12 +4,34 @@
 class Schema
   attr_reader :valid_schema
 
-  def initialize()
+  def initialize
     @valid_schema = { '-p' => ['port', Integer],
-    '-l' => ['logging', TrueClass],
-    '-d' => ['directory', String],
-    '-s' => ['server', String],
-    '-m' => ['make_directory', String],
-    '-a' => ['make_array', String] }
+                      '-l' => ['logging', TrueClass],
+                      '-d' => ['directory', String],
+                      '-s' => ['server', String],
+                      '-m' => ['make_directory', String],
+                      '-a' => ['make_array', String] }
+  end
+
+  def unpack_values(values)
+    prev_flag = false
+    final = []
+    values.each do |val|
+      break if !flag?(val) && !prev_flag
+
+      if flag?(val) && @valid_schema[val]
+        final.push(@valid_schema[val].unshift(val))
+        prev_flag = true
+      elsif prev_flag
+        val = val.to_i if final.last.last == Integer
+        final.last.append(val)
+        prev_flag = false
+      end
+    end
+    final
+  end
+
+  def flag?(arg)
+    arg.start_with?('-')
   end
 end
